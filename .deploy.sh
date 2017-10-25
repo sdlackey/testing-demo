ff768c3a268edc67b37114e6173d9141f5fea06c
sdlackey <sdl41996@aol.com>
2017-10-25 14:37:05 -0400

    Update .deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -11,11 +11,11 @@ git init
 
 # Configure Git
 git config user.name "Travis CI"
-git config user.email "nbartlett7@gmail.com"
+git config user.email "sdl41996@aol.com"
 
 # Commit all the things into the repo
 git add .
 git commit -m ":shipit: Deploy to GitHub Pages"
 
 # Force push to gh-pages
-git push --force "https://${GH_TOKEN}@github.com/tteltrab/testing-demo.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@github.com/sdlackey/testing-demo.git" master:gh-pages > /dev/null 2>&1
